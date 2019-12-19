a253d94d3351adb2a9c27d7ebd05a0823e8ddff4
corradopetrelli <corrado.petrelli@gmail.com>
2019-12-19 19:14:35 +0100

    Introduced route POST /users/login

--- src/routes/user/user.router.js
+++ src/routes/user/user.router.js
@@ -3,9 +3,12 @@ const UserController = require('./user.controller');
 
 const router = express.Router();
 
+// /users/
 router.get('/', UserController.getAll);
 router.post('/', UserController.register);
 router.delete('/', UserController.delete);
 router.put('/', UserController.update);
+// /users/login
+router.post('/login', UserController.login);
 
 module.exports = router;
