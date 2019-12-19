d6b8c8c543f76d78aee6990c071cc5d75e587ee4
Michele Biondi <michelebiondi01@gmail.com>
2019-12-19 16:51:42 +0100

    Improved models

--- src/routes/parkingSpot/parkingSpot.model.js
+++ src/routes/parkingSpot/parkingSpot.model.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const ParkingSpotSchema = new mongoose.Schema({
-  presence: Boolean,
-  latitude: Number,
-  longitude: Number,
-  handicapped: Boolean,
+  presence: {
+    type: Boolean,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+    min: -90,
+    max: 90,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+    min: -180,
+    max: 180,
+  },
+  handicapped: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('ParkingSpot', ParkingSpotSchema);
--- src/routes/user/user.model.js
+++ src/routes/user/user.model.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     unique: true,
     index: true,
+    required: true,
   },
   email: {
     type: String,
     unique: true,
     index: true,
+    required: true,
   },
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
   balance: {
     type: Number,
     default: 0.0,
